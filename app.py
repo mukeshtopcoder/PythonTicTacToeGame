c4b01cef0f8895a68549a242a4cd2a53eace4497
Mukesh <159673365+mukeshtopcoder@users.noreply.github.com>
2025-03-04 15:44:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,36 @@
-# TIC TAC TOE GAME
+# TIC_TAC_TOE
 
-place = [1,2,3,4,5,6,7,8,9]
-player = "X"
-choice = 0
-list1 = []
-while(len(list1)!=9):
-    print("\n\nTIC TAC TOE GAME\n\n")
-    print(" {} | {} | {} ".format(place[0],place[1],place[2]))
-    print("-----------")
-    print(" {} | {} | {} ".format(place[3],place[4],place[5]))
-    print("-----------")
-    print(" {} | {} | {} ".format(place[6],place[7],place[8]))
-    print("Player",player," : ",end="")
-    choice = int(input())
-
-    if(choice not in list1 and choice>0 and choice<10):
-        list1.append(choice)
-        place[choice-1] = player
-        if( (place[0]==place[1] and place[1]==place[2]) or (place[3]==place[4] and place[4]==place[5]) or (place[6]==place[7] and place[7]==place[8]) or (place[0]==place[3] and place[3]==place[6]) or (place[1]==place[4] and place[4]==place[7]) or (place[2]==place[5] and place[5]==place[8]) or (place[0]==place[4] and place[4]==place[8]) or (place[2]==place[4] and place[4]==place[6]) ):
-            print("\n\nPlayer "+player+" Win!")
-            print(" {} | {} | {} ".format(place[0],place[1],place[2]))
-            print("-----------")
-            print(" {} | {} | {} ".format(place[3],place[4],place[5]))
-            print("-----------")
-            print(" {} | {} | {} ".format(place[6],place[7],place[8]))
-            break
-        if(player=="X"):
-            player="0"
+li = [1,2,3,4,5,6,7,8,9]
+choice = []
+win = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
+player = 'X'
+flag = 1
+# Playboard
+while(True):
+    print("\n\t      ***** TIC-TAC-TOE *****\n")
+    print("\t\t",li[0]," | ",li[1]," | ",li[2])
+    print("\t\t----------------")
+    print("\t\t",li[3]," | ",li[4]," | ",li[5])
+    print("\t\t----------------")
+    print("\t\t",li[6]," | ",li[7]," | ",li[8])
+    if(flag == 0):
+        break
+    print("\n\t\tPlayer :",player)
+    if(len(choice)==9):
+        print("\n\n\t\tMATCH DRAW!")
+        break
+    ch = int(input("\t\tEnter Choice : "))
+    if(ch not in choice):
+        choice.append(ch)
+        li[ch-1] = player
+        for place in win:
+            if(li[place[0]] == li[place[1]] and li[place[1]] == li[place[2]] ):
+                print("\n\n\t\tPLAYER ",player,"WINS!")
+                flag = 0
+        if(player == 'X'):
+            player = '0'
         else:
-            player="X"
-else:
-    print("\n\nITS A DRAW MATCH!")
+            player = 'X'
+    else:
+        input("\t\tPlace Already Occupied!")
+    
